7bcede6800b0128ab8980bf99faa145e492fd64c
Spencer Strickland <spencerstricklandx@gmail.com>
2019-10-31 12:40:03 -0700

    Added gameplay picture for the readme

--- AWSupload.bat
+++ AWSupload.bat
@@ -1,3 +1,3 @@
 call aws s3 cp styles.css s3://my-bucket-test-spencer/cannon/styles.css --acl public-read
-call aws s3 cp scriptone.js s3://my-bucket-test-spencer/cannon/scriptone.js --acl public-read
+call aws s3 cp cannon_blaster.js s3://my-bucket-test-spencer/cannon/cannon_blaster.js --acl public-read
 call aws s3 cp index.html s3://my-bucket-test-spencer/cannon/index.html --acl public-read
